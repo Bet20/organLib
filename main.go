1857e3352bbbb85b5b863c8c7a56f612d41873e0
Bet20 <dcprodutionspt@gmail.com>
2021-12-11 15:23:54 +0000

    updated main

--- main.go
+++ main.go
@@ -2,32 +2,14 @@ package main
 
 import (
 	"fmt"
-	"organLib/files"
 	"organLib/generators"
 	"organLib/paths"
 )
 
 func main() {
-	r := paths.NewWithPath("D:\\docs\\pasta")
+	r := paths.NewWithPath(".")
 	// generators.SearchFileInOutputTree(r, "o.txt")
-	file, err := files.SearchFile(r, "o.txt")
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	file.CreateLogFile("testLocal.txt")
 	//files.DeleteFileInRoot(&file, r)
-	generators.CreateDirectoryWithTemplate(*r, `
-	d:pasta1
-	d:*pasta11
-	f:**pasta12.txt
-	d:**pp
-	d:**ii
-	d:***ty
-	f:****u.txt
-	f:****y.txt
-	d:pasta13.txt
-	d:pasta2
-	f:*pasta2.txt
-	`)
+	generators.SearchFileInOutputTree(r, "README.md")
 	fmt.Printf("Thanks for using organLib")
 }
